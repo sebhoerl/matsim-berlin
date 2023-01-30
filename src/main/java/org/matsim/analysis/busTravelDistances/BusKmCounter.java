fdff97310d1372a6d7109f59c5254f5b96848d97
Ruan J. Gräbe <rgraebe@vsp.tu-berlin.de>
2023-01-30 11:33:32 +0100

    Cleanup

--- src/main/java/org/matsim/analysis/busTravelDistances/BusKmCounter.java
+++ src/main/java/org/matsim/analysis/busTravelDistances/BusKmCounter.java
@@ -24,7 +24,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 
 /**
- * This class uses berlin-v5.5.3-1pct.output_events.xml.gz (or 10pct scenario)
+ * This class uses berlin-v5.5.3-1pct.output_events.xml.gz
  * to count the number of kms travelled by buses inside Berlin(-Brandenburg).
  *
  * @author rgraebe
@@ -32,36 +32,21 @@ import java.nio.file.Path;
 public class BusKmCounter implements VehicleLeavesTrafficEventHandler, LinkLeaveEventHandler {
 
     static Scenario scenario;
-
     String EVENTS_FILE_1pct = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.5-1pct/output-berlin-v5.5-1pct/berlin-v5.5.3-1pct.output_events.xml.gz";
-//    String EVENTS_FILE_10pct = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.5-10pct/output-berlinv5.5/berlin-v5.5.3-10pct.output_events.xml.gz";
     static String CONFIG_FILE_1pct = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.5-1pct/output-berlin-v5.5-1pct/berlin-v5.5.3-1pct.output_config.xml";
-//    static String CONFIG_FILE_10pct = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.5-10pct/output-berlinv5.5/berlin-v5.5.3-10pct.output_config.xml";
     static String VEHICLES_FILE_1pct = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.5-1pct/output-berlin-v5.5-1pct/berlin-v5.5.3-1pct.output_allVehicles.xml.gz";
-//    static String VEHICLES_FILE_10pct = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.5-10pct/output-berlinv5.5/berlin-v5.5.3-10pct.output_allVehicles.xml.gz";
-//    static String PLANS_FILE_10pct = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.5-10pct/output-berlinv5.5/berlin-v5.5.3-10pct.output_plans.xml.gz";
-    static String PLANS_FILE_1pct = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.5-1pct/output-berlin-v5.5-1pct/berlin-v5.5.3-1pct.output_plans.xml.gz";
-    static String NETWORK_FILE = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.5-10pct/output-berlinv5.5/berlin-v5.5.3-10pct.output_network.xml.gz";
-
+//    static String PLANS_FILE_1pct = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.5-1pct/output-berlin-v5.5-1pct/berlin-v5.5.3-1pct.output_plans.xml.gz";
+//    static String NETWORK_FILE = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.5-10pct/output-berlinv5.5/berlin-v5.5.3-10pct.output_network.xml.gz";
 
     static String SCENARIO_PCT = "1pct";
     Double BUS_KMS_BRANDENBURG = 0.;
     Double BUS_KMS_BERLIN = 0.;
     Double BUS_KMS_Total = 0.;
 	ShpOptions shpZones = new ShpOptions(Path.of("src/main/java/org/matsim/analysis/busTravelDistances/berlinBezirke/bezirksgrenzen.shp"), TransformationFactory.WGS84, StandardCharsets.UTF_8);
-	Index indexZones = shpZones.createIndex("EPSG:31468", "Gemeinde_n");
-
-//    private static final CoordinateTransformation transformation_fromEPSG31468 = TransformationFactory.getCoordinateTransformation("EPSG:31468", "EPSG:3857");
-    
-//    private static final Collection<SimpleFeature> features = ShapeFileReader.getAllFeatures("src/main/java/org/matsim/analysis/busTravelDistances/berlinBezirke/bezirksgrenzen.shp");
     // you can see the CRS of your shapefile in the .prj file that should be in the root where your .shp file is stored
-//    private static Geometry berlinGeometry = null;
-//    static GeometryFactory geofac = new GeometryFactory();
+    Index indexZones = shpZones.createIndex("EPSG:31468", "Gemeinde_n");
 
     public static void main( String[] args ) throws FactoryException, TransformException {
-
-//    	CreateBerlinGeometry(); // to read the shape file and covert the CRS's to be compatible with MATSim's links
-//        CountBerlinTrips(); // for testing only
 
         Config config = ConfigUtils.loadConfig( CONFIG_FILE_1pct );
         config.vehicles().setVehiclesFile( VEHICLES_FILE_1pct );
@@ -79,9 +64,7 @@ public class BusKmCounter implements VehicleLeavesTrafficEventHandler, LinkLeave
         System.out.println("Bus kms Brandenburg (" + SCENARIO_PCT + "): " + handler.BUS_KMS_BRANDENBURG );
         System.out.println("Bus kms Berlin (" + SCENARIO_PCT + "): " + handler.BUS_KMS_BERLIN );
         System.out.println("Bus kms Total (" + SCENARIO_PCT + "): " + handler.BUS_KMS_Total );
-
     }
-
 
     @Override
 	public void handleEvent(LinkLeaveEvent linkLeaveEvent) {
@@ -115,58 +98,5 @@ public class BusKmCounter implements VehicleLeavesTrafficEventHandler, LinkLeave
 //             not a transit vehicle OR vehicle = null :(
 //        }
     }
-
-//    private static void CountBerlinTrips() {
-//
-//        var population = PopulationUtils.readPopulation(PLANS_FILE_1pct);
-//        var network = NetworkUtils.readNetwork(NETWORK_FILE);
-////        var network = scenario.getNetwork();
-//
-//        int counter = 0;
-//
-//        for (var person : population.getPersons().values()) {
-//
-//            var plan = person.getSelectedPlan();
-//            var activities = TripStructureUtils.getActivities(plan, TripStructureUtils.StageActivityHandling.ExcludeStageActivities);
-//
-//            for (var i = 0 ; i < activities.size() - 1; i++) {
-//
-//                var fromCoord = getCoord(activities.get(i), network);
-//                var toCoord = getCoord(activities.get(i + 1), network);
-//
-//                if (isInGeometry(fromCoord, berlinGeometry) && isInGeometry(toCoord, berlinGeometry)) {
-//                    counter++;
-//                }
-//            }
-//        }
-////        System.out.println("========== " + counter + " trips within Berlin ==========");
-//
-//    }
-
-//    private static void CreateBerlinGeometry() throws FactoryException, TransformException {
-//        // one big geometry that contains all Berlin's districts
-//        berlinGeometry = features.stream()
-//                .map(feature -> (Geometry) feature.getDefaultGeometry())
-//                .findAny().orElseThrow();
-//
-//        // to match the shapefile's CRS with Berlin's CRS
-//        CoordinateReferenceSystem sourceCRS = MGC.getCRS(TransformationFactory.WGS84);
-//        CoordinateReferenceSystem targetCRS = MGC.getCRS("EPSG:3857");
-//        MathTransform transform = CRS.findMathTransform(sourceCRS, targetCRS);
-//        berlinGeometry = JTS.transform(berlinGeometry, transform);
-//    }
-
-//    private static Coord getCoord( Activity activity, Network network) {
-//        if (activity.getCoord() != null) {
-//            return activity.getCoord();
-//        }
-//        return network.getLinks().get(activity.getLinkId()).getCoord();
-//    }
-
-//    private static boolean isInGeometry(Coord coord, Geometry geometry) {
-//        var transformed = transformation_fromEPSG31468.transform(coord);
-//        return geometry.covers( MGC.coord2Point(transformed));
-////        return geometry.covers( MGC.coord2Point(coord));
-//    }
 
 }
